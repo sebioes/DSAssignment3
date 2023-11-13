d58989154750bbfaee0adb6426403807cb6b6280
sebioes <121304225+sebioes@users.noreply.github.com>
2023-11-13 18:17:15 +0100

    Update WordCount.java
    added comments, print to master terminal

--- tasks/app/src/main/java/com/assignment3/spark/WordCount.java
+++ tasks/app/src/main/java/com/assignment3/spark/WordCount.java
@@ -31,7 +31,7 @@ public class WordCount {
     }
 
     public static void main(String[] args) {
-        String textFilePath = "input/pigs.txt"; // update to HDFS url for task2
+        String textFilePath = "/Users/nicolas.montani/Home/Code/Github/UNI/DS/DSAssignment3/tasks/input/pigs.txt"; // update to HDFS url for task2
         SparkConf conf = new SparkConf().setAppName("WordCountWithSpark").setMaster("local[*]"); // task2: update the setMaster with your cluster master URL for executing this code on the cluster
         JavaSparkContext sparkContext =  new JavaSparkContext(conf);
         JavaRDD<String> textFile = sparkContext.textFile(textFilePath);
@@ -45,12 +45,14 @@ public class WordCount {
          * print the word count and save the output in the format, e.g.,(in:15) to an 'output' folder (on HDFS for task 2)
          * try to consolidate your output into single text file if you want to check your output against the given sample output
          */
-
+        //counts word occurrences using Spark's mapToPair and reduceByKey
         JavaPairRDD<String, Integer> counts = words.mapToPair(word -> new Tuple2<>(word, 1)).reduceByKey((a, b) -> a + b);
+        //swaps key-value pairs, sorts by key in descending order, and swaps back
         counts = counts.mapToPair(x -> x.swap()).sortByKey(false).mapToPair(x -> x.swap());
-        //counts.saveAsTextFile("hdfs://172.20.10.2:9000/sparkApp/input/output.txt");
-        //saves the partitions as one file
-        counts.coalesce(1).saveAsTextFile("hdfs://172.20.10.2:9000/sparkApp/input/output.txt")
+        //print to master terminal
+        counts.foreach(e-> System.out.println(e));
+        //output consolidation
+        counts.coalesce(1).saveAsTextFile("/Users/nicolas.montani/Home/Code/Github/UNI/DS/DSAssignment3/tasks/input/output-task1.txt");
 
 
 
